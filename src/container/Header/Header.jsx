255d30ab476b852ba33a04a2c42fe4ce66327933
akramgadir <akram.gadir@gmail.com>
2023-12-10 19:05:40 +0000

    about us section complete

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import './Header.css';
-import images from '../../constants/images';
+
 import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Header.css';
 
 const Header = () => (
-  //give it an id of home so we can scroll to it
-  <>
-  <div className='app__header app__wrapper section__padding' id='home'>
-    <div className='app_wrapper_info'>
-    <SubHeading title='Chase The New Flavour'/>
-    <h1 className='app__header-h1'>The Key to Fine Dining</h1>
-    <p className='p__opensans' style={{margin:'2rem 0'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    <button className='custom__button'>Explore Menu</button>
-    </div> 
-  </div>
-  <div className='app__header_img'>
-    <img src={images.welcome} alt="header-img" />
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Explore new flavours" />
+      <h1 className="app__header-h1">The Key To Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+      <button type="button" className="custom__button">Explore Menu</button>
+    </div>
 
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="Header salmon" />
+    </div>
   </div>
-</>
 );
 
 export default Header;
