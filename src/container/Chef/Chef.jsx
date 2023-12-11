e621af00e019295028f34a1321eba91facb5bbef
akramgadir <akram.gadir@gmail.com>
2023-12-11 18:46:06 +0000

    chef section in progress

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-
 import './Chef.css';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper'>
+    
+  <div className='app__wrapper_img app__wrapper_img-reverse'>
+    <img src={images.chef} alt="chef" />
+  </div>
+
+  <div className='app__wrapper_info'>
+    <SubHeading title='Chef&apos;s Word'/>
+  </div>
+
   </div>
 );
 
